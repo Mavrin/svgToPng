dbcd6db0dc9ac2e88a0349b724baae5b3e5893b4
konstantin <krivlenia@targetprocess.com>
2015-04-05 19:34:29 +0300

    add example

---
+++ moviesData.js
@@ -0,0 +1,583 @@
+var moviesData = [
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 2,
+        "WEEKEND_PER_THEATER": 13838,
+        "WEEKEND_DATE": "9/24/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 3,
+        "WEEKEND_PER_THEATER": 11599,
+        "WEEKEND_DATE": "10/1/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 4,
+        "WEEKEND_PER_THEATER": 7753,
+        "WEEKEND_DATE": "10/8/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 5,
+        "WEEKEND_PER_THEATER": 4895,
+        "WEEKEND_DATE": "10/15/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 6,
+        "WEEKEND_PER_THEATER": 3551,
+        "WEEKEND_DATE": "10/22/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 7,
+        "WEEKEND_PER_THEATER": 2474,
+        "WEEKEND_DATE": "10/29/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 8,
+        "WEEKEND_PER_THEATER": 2141,
+        "WEEKEND_DATE": "11/5/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 9,
+        "WEEKEND_PER_THEATER": 1876,
+        "WEEKEND_DATE": "11/12/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 10,
+        "WEEKEND_PER_THEATER": 1587,
+        "WEEKEND_DATE": "11/19/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 11,
+        "WEEKEND_PER_THEATER": 2001,
+        "WEEKEND_DATE": "11/26/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 12,
+        "WEEKEND_PER_THEATER": 1124,
+        "WEEKEND_DATE": "12/3/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 13,
+        "WEEKEND_PER_THEATER": 1014,
+        "WEEKEND_DATE": "12/10/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 14,
+        "WEEKEND_PER_THEATER": 1167,
+        "WEEKEND_DATE": "12/17/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 15,
+        "WEEKEND_PER_THEATER": 1099,
+        "WEEKEND_DATE": "12/24/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 16,
+        "WEEKEND_PER_THEATER": 1815,
+        "WEEKEND_DATE": "12/31/1999"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 17,
+        "WEEKEND_PER_THEATER": 1998,
+        "WEEKEND_DATE": "1/7/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 18,
+        "WEEKEND_PER_THEATER": 2168,
+        "WEEKEND_DATE": "1/14/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 19,
+        "WEEKEND_PER_THEATER": 1614,
+        "WEEKEND_DATE": "1/21/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 20,
+        "WEEKEND_PER_THEATER": 2047,
+        "WEEKEND_DATE": "1/28/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 21,
+        "WEEKEND_PER_THEATER": 7523,
+        "WEEKEND_DATE": "2/4/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 22,
+        "WEEKEND_PER_THEATER": 9185,
+        "WEEKEND_DATE": "2/11/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 23,
+        "WEEKEND_PER_THEATER": 5132,
+        "WEEKEND_DATE": "2/18/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 24,
+        "WEEKEND_PER_THEATER": 3483,
+        "WEEKEND_DATE": "2/25/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 25,
+        "WEEKEND_PER_THEATER": 3010,
+        "WEEKEND_DATE": "3/3/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 26,
+        "WEEKEND_PER_THEATER": 2408,
+        "WEEKEND_DATE": "3/10/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 27,
+        "WEEKEND_PER_THEATER": 1902,
+        "WEEKEND_DATE": "3/17/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 28,
+        "WEEKEND_PER_THEATER": 2422,
+        "WEEKEND_DATE": "3/24/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 29,
+        "WEEKEND_PER_THEATER": 2717,
+        "WEEKEND_DATE": "3/31/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 30,
+        "WEEKEND_PER_THEATER": 1731,
+        "WEEKEND_DATE": "4/7/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 31,
+        "WEEKEND_PER_THEATER": 1341,
+        "WEEKEND_DATE": "4/14/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 32,
+        "WEEKEND_PER_THEATER": 1201,
+        "WEEKEND_DATE": "4/21/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 33,
+        "WEEKEND_PER_THEATER": 848,
+        "WEEKEND_DATE": "4/28/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 34,
+        "WEEKEND_PER_THEATER": 711,
+        "WEEKEND_DATE": "5/5/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 35,
+        "WEEKEND_PER_THEATER": 724,
+        "WEEKEND_DATE": "5/12/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 36,
+        "WEEKEND_PER_THEATER": 640,
+        "WEEKEND_DATE": "5/19/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 37,
+        "WEEKEND_PER_THEATER": 827,
+        "WEEKEND_DATE": "5/26/2000"
+    },
+    {
+        "NUMBER": 2,
+        "MOVIE": "American Beauty",
+        "WEEK_NUM": 38,
+        "WEEKEND_PER_THEATER": 479,
+        "WEEKEND_DATE": "6/2/2000"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 1,
+        "WEEKEND_PER_THEATER": 18462,
+        "WEEKEND_DATE": "6/23/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 2,
+        "WEEKEND_PER_THEATER": 13664,
+        "WEEKEND_DATE": "6/30/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 3,
+        "WEEKEND_PER_THEATER": 8745,
+        "WEEKEND_DATE": "7/7/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 4,
+        "WEEKEND_PER_THEATER": 6867,
+        "WEEKEND_DATE": "7/14/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 5,
+        "WEEKEND_PER_THEATER": 5072,
+        "WEEKEND_DATE": "7/21/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 6,
+        "WEEKEND_PER_THEATER": 4068,
+        "WEEKEND_DATE": "7/28/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 7,
+        "WEEKEND_PER_THEATER": 3235,
+        "WEEKEND_DATE": "8/4/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 8,
+        "WEEKEND_PER_THEATER": 2829,
+        "WEEKEND_DATE": "8/11/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 9,
+        "WEEKEND_PER_THEATER": 2457,
+        "WEEKEND_DATE": "8/18/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 10,
+        "WEEKEND_PER_THEATER": 1995,
+        "WEEKEND_DATE": "8/25/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 11,
+        "WEEKEND_PER_THEATER": 2634,
+        "WEEKEND_DATE": "9/1/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 12,
+        "WEEKEND_PER_THEATER": 1470,
+        "WEEKEND_DATE": "9/8/1989"
+    },
+    {
+        "NUMBER": 3,
+        "MOVIE": "Batman",
+        "WEEK_NUM": 13,
+        "WEEKEND_PER_THEATER": 1363,
+        "WEEKEND_DATE": "9/15/1989"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 1,
+        "WEEKEND_PER_THEATER": 9931,
+        "WEEKEND_DATE": "12/7/1984"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 2,
+        "WEEKEND_PER_THEATER": 7516,
+        "WEEKEND_DATE": "12/14/1984"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 3,
+        "WEEKEND_PER_THEATER": 7825,
+        "WEEKEND_DATE": "12/21/1984"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 4,
+        "WEEKEND_PER_THEATER": 10002,
+        "WEEKEND_DATE": "12/28/1984"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 5,
+        "WEEKEND_PER_THEATER": 5741,
+        "WEEKEND_DATE": "1/4/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 6,
+        "WEEKEND_PER_THEATER": 4857,
+        "WEEKEND_DATE": "1/11/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 7,
+        "WEEKEND_PER_THEATER": 3397,
+        "WEEKEND_DATE": "1/18/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 8,
+        "WEEKEND_PER_THEATER": 4510,
+        "WEEKEND_DATE": "1/25/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 9,
+        "WEEKEND_PER_THEATER": 4187,
+        "WEEKEND_DATE": "2/1/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 10,
+        "WEEKEND_PER_THEATER": 3617,
+        "WEEKEND_DATE": "2/8/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 11,
+        "WEEKEND_PER_THEATER": 5052,
+        "WEEKEND_DATE": "2/15/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 12,
+        "WEEKEND_PER_THEATER": 3866,
+        "WEEKEND_DATE": "2/22/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 13,
+        "WEEKEND_PER_THEATER": 3451,
+        "WEEKEND_DATE": "3/1/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 14,
+        "WEEKEND_PER_THEATER": 3004,
+        "WEEKEND_DATE": "3/8/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 15,
+        "WEEKEND_PER_THEATER": 3139,
+        "WEEKEND_DATE": "3/15/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 16,
+        "WEEKEND_PER_THEATER": 2847,
+        "WEEKEND_DATE": "3/22/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 17,
+        "WEEKEND_PER_THEATER": 2618,
+        "WEEKEND_DATE": "3/29/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 18,
+        "WEEKEND_PER_THEATER": 2838,
+        "WEEKEND_DATE": "4/5/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 19,
+        "WEEKEND_PER_THEATER": 2470,
+        "WEEKEND_DATE": "4/12/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 20,
+        "WEEKEND_PER_THEATER": 2212,
+        "WEEKEND_DATE": "4/19/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 21,
+        "WEEKEND_PER_THEATER": 2171,
+        "WEEKEND_DATE": "4/26/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 22,
+        "WEEKEND_PER_THEATER": 2007,
+        "WEEKEND_DATE": "5/3/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 23,
+        "WEEKEND_PER_THEATER": 1878,
+        "WEEKEND_DATE": "5/10/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 24,
+        "WEEKEND_PER_THEATER": 2063,
+        "WEEKEND_DATE": "5/17/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 25,
+        "WEEKEND_PER_THEATER": 2322,
+        "WEEKEND_DATE": "5/24/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 26,
+        "WEEKEND_PER_THEATER": 1430,
+        "WEEKEND_DATE": "5/31/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 27,
+        "WEEKEND_PER_THEATER": 1364,
+        "WEEKEND_DATE": "6/7/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 28,
+        "WEEKEND_PER_THEATER": 1560,
+        "WEEKEND_DATE": "6/14/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 29,
+        "WEEKEND_PER_THEATER": 1667,
+        "WEEKEND_DATE": "6/21/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 30,
+        "WEEKEND_PER_THEATER": 1457,
+        "WEEKEND_DATE": "6/28/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 31,
+        "WEEKEND_PER_THEATER": 1672,
+        "WEEKEND_DATE": "7/5/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 32,
+        "WEEKEND_PER_THEATER": 1568,
+        "WEEKEND_DATE": "7/12/1985"
+    },
+    {
+        "NUMBER": 4,
+        "MOVIE": "Beverly Hills Cop",
+        "WEEK_NUM": 33,
+        "WEEKEND_PER_THEATER": 1676,
+        "WEEKEND_DATE": "7/19/1985"
+    }
+]
